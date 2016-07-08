5d1073817507e4be42c4ad35c892fcf8712eb39b
Lee Watson <rev@revthefox.co.uk>
2016-07-09 00:56:21 +0100

    Make mpv geometry better suited for dual monitors and fix fish paths not loading before X

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,9 @@
 ## /etc/profile compatibility
 env -i HOME=$HOME dash -l -c 'export -p' | sed -e "/PWD/d; /PATH/s/'//g;/PATH/s/:/ /g;s/=/ /;s/^export/set -x/" | source
 
+## PATH
+set -gx fish_user_paths $HOME/.cargo/bin $HOME/.fzf/bin $HOME/.composer/vendor/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin /usr/lib/ccache/bin $HOME/.local/bin $fish_user_paths
+
 if status --is-login
     if test -z "$DISPLAY" -a $XDG_VTNR -eq 1 -a -z "$SSH_CONNECTION"
         exec startx > /dev/null ^&1
@@ -23,9 +26,6 @@ set -gx LC_ALL en_GB.UTF-8
 ## Default programs
 set -gx BROWSER '/usr/bin/firejail firefox'
 set -gx EDITOR /usr/bin/vim
-
-## PATH
-set -gx fish_user_paths $HOME/.cargo/bin $HOME/.fzf/bin $HOME/.composer/vendor/bin $HOME/.cargo/bin $HOME/.local/bin $HOME/.gem/ruby/2.1.0/bin $HOME/.gem/ruby/2.0.0/bin /usr/lib/ccache/bin $HOME/.local/bin $fish_user_paths
 
 ## XDG
 set -gx XDG_CONFIG_HOME $HOME/.config
