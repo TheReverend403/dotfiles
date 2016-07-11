a27839f713014e4998558bf90b6fb24c907fbbb2
Lee Watson <rev@revthefox.co.uk>
2016-07-11 04:40:36 +0100

    Don't create WINE function if WINE isn't actually installed

--- config/fish/functions/wine.fish
+++ config/fish/functions/wine.fish
@@ -1,7 +1,9 @@
-function wine -d 'WINE with OpenGL optimisation environment variables.'
-    set -x __GL_THREADED_OPTIMIZATIONS 0
-    set -x __GL_SYNC_TO_VBLANK 1
-    set -x __GL_YIELD 'NOTHING'
-    set -x STAGING_SHARED_MEMORY 1
-    command wine $argv
+if test -x (which wine)
+    function wine -d 'WINE with OpenGL optimisation environment variables.'
+        set -x __GL_THREADED_OPTIMIZATIONS 0
+        set -x __GL_SYNC_TO_VBLANK 1
+        set -x __GL_YIELD 'NOTHING'
+        set -x STAGING_SHARED_MEMORY 1
+        command wine $argv
+    end
 end
