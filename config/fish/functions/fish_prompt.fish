4b435b0d267568cca46394997a303d3107fee8d0
Lee Watson <rev@revthefox.co.uk>
2016-07-11 02:47:14 +0100

    Make prompt hostname short

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,6 +1,12 @@
 function fish_prompt
     set last_status $status
-    printf '%s@%s ' (whoami) (hostname)
+
+    # Just calculate this once, to save a few cycles when displaying the prompt.
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname -s)
+    end
+
+    printf '%s@%s ' (whoami) "$__fish_prompt_hostname"
 
     set_color $fish_color_cwd
     printf '%s' (prompt_pwd)
