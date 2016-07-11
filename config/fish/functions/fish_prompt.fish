699e0addeeeef56452709670dbb7932086deec32
Lee Watson <rev@revthefox.co.uk>
2016-07-11 03:22:25 +0100

    One less call to a shell command

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt
     set last_status $status
 
-    printf '%s@%s ' (whoami) (hostname -s)
+    printf '%s@%s ' $USER (hostname -s)
 
     set_color $fish_color_cwd
     printf '%s' (prompt_pwd)
