8d79dd54fe9a79c3f009c508486645da49ff5f43
Lee Watson <rev@revthefox.co.uk>
2016-07-17 03:10:33 +0100

    Add sudo and ssh labels to fish_prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,6 +1,18 @@
 function fish_prompt
     set last_status $status
 
+    if test -n "$SSH_CLIENT"
+        set_color magenta
+        printf 'ssh:'
+        set_color normal
+    end
+
+    if test -n "$SUDO_USER"
+        set_color red
+        printf 'sudo:'
+        set_color normal
+    end
+
     printf '%s@%s ' $USER (hostname -s)
 
     set_color $fish_color_cwd
