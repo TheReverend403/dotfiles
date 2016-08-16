b79b881688d2d06e464f22b7e4aa7f6a8e1e82e8
Lee Watson <rev@revthefox.co.uk>
2016-08-16 23:05:04 +0100

    update less colours

--- config/fish/functions/man.fish
+++ config/fish/functions/man.fish
@@ -1,10 +1,10 @@
 function man
-    set -x LESS_TERMCAP_mb (printf "\e[1;31m")
-    set -x LESS_TERMCAP_md (printf "\e[1;31m")
-    set -x LESS_TERMCAP_me (printf "\e[0m")
-    set -x LESS_TERMCAP_se (printf "\e[0m")
-    set -x LESS_TERMCAP_so (printf "\e[1;44;33m")
-    set -x LESS_TERMCAP_ue (printf "\e[0m")
-    set -x LESS_TERMCAP_us (printf "\e[1;32m")
+    set -x LESS_TERMCAP_mb (tput bold; tput setaf 5)
+    set -x LESS_TERMCAP_md (tput bold; tput setaf 5)
+    set -x LESS_TERMCAP_me (tput sgr0)
+    set -x LESS_TERMCAP_so (tput bold; tput setaf 5; tput setab 0)
+    set -x LESS_TERMCAP_se (tput rmso; tput sgr0)
+    set -x LESS_TERMCAP_ue (tput rmul; tput sgr0)
+    set -x LESS_TERMCAP_us (tput smul; tput bold; tput setaf 7)
     command man $argv
 end
