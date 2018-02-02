0e9c9dfeb5cd46d77fc74cc1b71c7eac6913c254
Lee Watson <rev@revthefox.co.uk>
2018-02-02 22:06:29 +0000

    Let fisher pyenv handle PYENV_ROOT

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -53,7 +53,6 @@ if status --is-login
 
     ## Python
     set -x VIRTUAL_ENV_DISABLE_PROMPT true
-    set -x PYENV_ROOT "$HOME/.pyenv"
 
     ## NodeJS
     set -x NPM_PACKAGES "$HOME/.npm-packages"
@@ -63,7 +62,7 @@ if status --is-login
     set -x GOPATH "$HOME/.go"
 
     ## PATH
-    set -l user_dirs "$PYENV_ROOT/bin" "$GOPATH/bin" "$NPM_PACKAGES/bin" "$HOME/.composer/vendor/bin" "$HOME/.cargo/bin" "$HOME/.local/bin"
+    set -l user_dirs "$HOME/.pyenv/bin" "$GOPATH/bin" "$NPM_PACKAGES/bin" "$HOME/.composer/vendor/bin" "$HOME/.cargo/bin" "$HOME/.local/bin"
 
     ## Only add items to $PATH that actually exist. Prevents fish complaining.
     for dir in $user_dirs
