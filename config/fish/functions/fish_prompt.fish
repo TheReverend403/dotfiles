1871ef02a0d848f4c67ac0fc09470ae5abbf8e83
Lee Watson <rev@revthefox.co.uk>
2019-09-25 15:45:29 +0100

    Italicise user+host in fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,19 +1,21 @@
 function fish_prompt
     if test -n "$SSH_CLIENT"
         set_color -i green
-        printf 'ssh:'
+        printf '(ssh)'
     end
 
     if test -n "$SUDO_USER"
         set_color -i red
-        printf 'sudo:'
+        printf '(root)'
     end
 
     set_color normal
+    set_color -i
     printf '%s@%s ' $USER (hostname -s)
+    set_color normal
 
     set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
+    printf "%s" (prompt_pwd)
 
     set_color normal
     printf '> '
