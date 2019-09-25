bfe86e9d64ba681535101713c80a82d1925becdf
Lee Watson <rev@revthefox.co.uk>
2019-09-25 16:24:54 +0100

    Make fish_color_end bold

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,7 +15,7 @@ set fish_color_param default
 set fish_color_command default 
 set fish_color_cwd magenta
 set fish_color_operator default --bold
-set fish_color_end white
+set fish_color_end default --bold
 set fish_color_comment brblack --italics
 set fish_color_quote bryellow
 set fish_color_error red
