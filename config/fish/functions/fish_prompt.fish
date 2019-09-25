7f75d71219592f4012d5a1b7fad4c00fd13d1d98
Lee Watson <rev@revthefox.co.uk>
2019-09-25 16:08:49 +0100

    Fix spacing on prompt prefixes

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,16 +1,15 @@
 function fish_prompt
     if test -n "$SSH_CLIENT"
         set_color -i green
-        printf '(ssh)'
+        printf '(ssh) '
     end
 
-    if test -n "$SUDO_USER"
+    if test (id -u) -eq 0
         set_color -i red
-        printf '(root)'
+        printf '(root) '
     end
 
-    set_color normal
-    set_color -i
+    set_color normal; set_color -i
     printf '%s@%s ' $USER (hostname -s)
     set_color normal
 
