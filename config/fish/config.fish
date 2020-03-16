7d0d7dff54171490b57fa9c6f8df2da951f595f3
Lee Watson <rev@revthefox.co.uk>
2020-03-16 15:48:23 +0000

    Only start X on tty1

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -70,7 +70,7 @@ test -s "$__fish_config_dir/local.fish"; and source "$__fish_config_dir/local.fi
 
 if status is-login
     # Run X if not already running, not root, not in SSH and not in tmux
-    if test -z "$DISPLAY" -a (id -u "$USER") -ne 0 -a -z "$SSH_CLIENT" -a -z "$TMUX"
+    if test -z "$DISPLAY" -a (id -u "$USER") -ne 0 -a -z "$SSH_CLIENT" -a -z "$TMUX" -a (tty) = "/dev/tty1"
         if available startx
             exec startx >/dev/null ^&1
         end
