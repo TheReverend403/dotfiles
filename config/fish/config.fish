fe2da45822495bef99cd947e2dbf72a21198da12
Lee Watson <rev@revthefox.co.uk>
2020-03-16 16:52:25 +0000

    Make redirection work on fish 3.1 properly

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -72,7 +72,7 @@ if status is-login
     # Run X if not already running, not root, not in SSH and not in tmux
     if test -z "$DISPLAY" -a (id -u "$USER") -ne 0 -a -z "$SSH_CLIENT" -a -z "$TMUX" -a (tty) = "/dev/tty1"
         if available startx
-            exec startx >/dev/null ^&1
+            exec startx > /dev/null 2>&1
         end
     end
 end
