f17db0a278fe7e7c0203d10e93ecb7a4a0201133
Lee Watson <rev@revthefox.co.uk>
2021-07-02 22:50:47 +0100

    Move lots of things to XDG dirs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -47,10 +47,26 @@ set -x ASPELL_CONF "per-conf $XDG_CONFIG_HOME/aspell/aspell.conf; personal $XDG_
 set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -x CUDA_CACHE_PATH "$XDG_CACHE_HOME/nv"
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
-set -x NODE_REPL_HISTORY "$XDG_DATA_HOME/node_repl_history"
+set -x NODE_REPL_HISTORY "$XDG_CACHE_HOME/node_repl_history"
+set -x SQLITE_HISTORY "$XDG_CACHE_HOME/sqlite_history"
+set -x PSQLRC "$XDG_CONFIG_HOME/pg/psqlrc"
+set -x PGSERVICEFILE "$XDG_CONFIG_HOME/pg/pg_service.conf"
+set -x PGPASSFILE "$XDG_CONFIG_HOME/pg/pgpass"
+set -x PSQL_HISTORY "$XDG_CACHE_HOME/pg/psql_history"
+set -x GRADLE_USER_HOME "$XDG_DATA_HOME/gradle"
+set -x IPYTHONDIR "$XDG_CONFIG_HOME/jupyter"
+set -x JUPYTER_CONFIG_DIR "$IPYTHONDIR"
+set -x _JAVA_OPTIONS -Djava.util.prefs.userRoot="$XDG_CONFIG_HOME/java"
+set -x GTK2_RC_FILES "$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+set -x WGETRC "$XDG_CONFIG_HOME/wget/wgetrc"
+
+# Weechat 3.2 implements XDG properly.
+if available weechat; and test (weechat -v) -lt "3.2"
+    set -x WEECHAT_HOME "$XDG_CONFIG_HOME/weechat"
+end
 
 ## PATH
-set -U fish_user_paths "$HOME/.local/bin" "$HOME/.poetry/bin" "$HOME/.local/share/cargo/bin" "$GOPATH/bin" "$NPM_PACKAGES/bin" "$HOME/.config/composer/vendor/bin" "/usr/local/sbin" "/usr/local/bin"
+set -U fish_user_paths "$HOME/.local/bin" "$XDG_DATA_HOME/poetry/bin" "$CARGO_HOME/bin" "$GOPATH/bin" "$NPM_PACKAGES/bin" "$HOME/.config/composer/vendor/bin" "/usr/local/sbin" "/usr/local/bin"
 
 ## Aliases
 test -s "$__fish_config_dir/aliases.fish"; and source "$__fish_config_dir/aliases.fish"
