5fa4cbf35b4a7e4ac1fb2e0f0829129beb99f12f
Lee Watson <me@lwatson.dev>
2021-11-15 18:56:41 +0000

    Pull in user.js settings from firefox-ui-fix

--- firefox/user.js
+++ firefox/user.js
@@ -18,10 +18,31 @@ user_pref('font.name-list.emoji', 'emoji');
 user_pref('browser.tabs.allowTabDetach', false);
 
 // Theming
+// https://github.com/black7375/Firefox-UI-Fix/blob/photon-style/user.js
+
+// userchrome.css usercontent.css activate
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// https://github.com/black7375/Firefox-UI-Fix/blob/photon-style/user.js
+// Proton Enabled #127
 user_pref("browser.proton.enabled", true);
+
+// Proton Tooltip
+user_pref("browser.proton.places-tooltip.enabled", true);
+
+// Fill SVG Color
 user_pref("svg.context-properties.content.enabled", true);
+
+// CSS Color Mix - 88 Above
+user_pref("layout.css.color-mix.enabled", true);
+
+// CSS Blur Filter - 88 Above
 user_pref("layout.css.backdrop-filter.enabled", true);
+
+// Restore Compact Mode - 89 Above
 user_pref("browser.compactmode.show", true);
+
+// about:home Search Bar - 89 Above
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);
+
+// Browser Theme Based Scheme - Will be activate 95 Above
+// user_pref("layout.css.prefers-color-scheme.content-override", 3);
