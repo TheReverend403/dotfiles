473c9a105e372e408494dfa9d9efeacf6320fe19
Lee Watson <me@lwatson.dev>
2022-04-28 08:51:18 +0100

    Update firefox stuff for Firefox-UI-Fix 5

---
+++ firefox/user-overrides.js
@@ -0,0 +1,29 @@
+
+//
+// Overrides
+//
+
+// Disable global menu on Alt
+user_pref('ui.key.menuAccessKey', 0);
+
+// Disable clipboard annoyances
+user_pref('middlemouse.contentLoadURL', false);
+user_pref('clipboard.autocopy', false);
+
+// Don't ever start with Windows.
+user_pref('toolkit.winRegisterApplicationRestart', false);
+
+// ???
+user_pref('browser.formfill.enable', false);
+
+// Use system native emoji
+user_pref('font.name-list.emoji', 'emoji');
+
+// Disable this annoying thing which I never actually use but constantly trigger.
+user_pref('browser.tabs.allowTabDetach', false);
+
+user_pref('extensions.pocket.enabled', false);
+
+// Custom theming options
+user_pref("userChrome.theme.proton_color.dark_blue_accent", true);
+
