8e22e7ac336abb2839a559e36d64bca661111691
Lee Watson <me@lwatson.dev>
2022-07-01 00:05:37 +0100

    Add XDG support to npm

--- config/fish/conf.d/10-env.fish
+++ config/fish/conf.d/10-env.fish
@@ -13,7 +13,6 @@ set -x RUSTUP_HOME "$XDG_DATA_HOME/rustup"
 # XDG partial support.
 set -x ASPELL_CONF "per-conf $XDG_CONFIG_HOME/aspell/aspell.conf; personal $XDG_CONFIG_HOME/aspell/en.pws; repl $XDG_CONFIG_HOME/aspell/en.prepl"
 set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
-set -x CUDA_CACHE_PATH "$XDG_CACHE_HOME/nv"
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
 set -x NODE_REPL_HISTORY "$XDG_CACHE_HOME/node_repl_history"
 set -x SQLITE_HISTORY "$XDG_CACHE_HOME/sqlite_history"
@@ -29,6 +28,9 @@ set -x INPUTRC "$XDG_CONFIG_HOME/readline/inputrc"
 set -x MYSQL_HISTFILE "$XDG_DATA_HOME/mysql_history"
 set -x LESSHISTFILE -
 set -x PARALLEL_HOME "$XDG_CONFIG_HOME/parallel"
+set -x NPM_PACKAGES "$XDG_DATA_HOME/npm"
 
 ## PATH
 set -U fish_user_paths "$HOME/.local/bin" "$XDG_DATA_HOME/poetry/bin" "$CARGO_HOME/bin" "$GOPATH/bin" "$NPM_PACKAGES/bin" "$HOME/.config/composer/vendor/bin" "/usr/local/sbin" "/usr/local/bin"
+
+set -x GTK_THEME Yaru-Lavender-dark
