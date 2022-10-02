2176e34aaed53f172cf882999e350c4eb9f554d0
Lee Watson <me@lwatson.dev>
2022-10-02 10:08:21 +0100

    wtf I love fish_add_path now

--- config/fish/conf.d/10-env.fish
+++ config/fish/conf.d/10-env.fish
@@ -1,23 +1,32 @@
-## Prevent Wine from adding menu entries and desktop links.
+## Prevent Wine adding menu entries and desktop links.
 set -x WINEDLLOVERRIDES 'winemenubuilder.exe=d'
-
 
 ## Python
 set -x VIRTUAL_ENV_DISABLE_PROMPT true
-set -x POETRY_HOME "$XDG_DATA_HOME/poetry"
 set -x IPYTHONDIR "$XDG_CONFIG_HOME/jupyter"
 set -x JUPYTER_CONFIG_DIR "$IPYTHONDIR"
+set -x POETRY_HOME "$XDG_DATA_HOME/poetry"
+fish_add_path "$POETRY_HOME/bin"
 
 # Go
 set -x GOPATH "$XDG_DATA_HOME/go"
+fish_add_path "$GOPATH/bin"
 
 # Rust
 set -x RUSTUP_HOME "$XDG_DATA_HOME/rustup"
 set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
+fish_add_path "$CARGO_HOME/bin"
+
+# Node
+set -x NPM_PACKAGES "$XDG_DATA_HOME/npm"
+set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
+fish_add_path "$NPM_PACKAGES/bin"
+
+# PHP
+fish_add_path "$XDG_CONFIG_HOME/composer/vendor/bin"
 
 # XDG partial support.
 set -x ASPELL_CONF "per-conf $XDG_CONFIG_HOME/aspell/aspell.conf; personal $XDG_CONFIG_HOME/aspell/en.pws; repl $XDG_CONFIG_HOME/aspell/en.prepl"
-set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
 set -x NODE_REPL_HISTORY "$XDG_CACHE_HOME/node_repl_history"
 set -x SQLITE_HISTORY "$XDG_CACHE_HOME/sqlite_history"
 set -x PSQLRC "$XDG_CONFIG_HOME/pg/psqlrc"
@@ -30,7 +39,7 @@ set -x INPUTRC "$XDG_CONFIG_HOME/readline/inputrc"
 set -x MYSQL_HISTFILE "$XDG_DATA_HOME/mysql_history"
 set -x LESSHISTFILE -
 set -x PARALLEL_HOME "$XDG_CONFIG_HOME/parallel"
-set -x NPM_PACKAGES "$XDG_DATA_HOME/npm"
 
-## PATH
-fish_add_path -p "$HOME/.local/bin" "$XDG_DATA_HOME/poetry/bin" "$CARGO_HOME/bin" "$GOPATH/bin" "$NPM_PACKAGES/bin" "$XDG_CONFIG_HOME/composer/vendor/bin" "/usr/local/sbin" "/usr/local/bin"
+# Use --move here so ~/.local/bin is always used first and /usr/* is always last.
+fish_add_path --move --append "/usr/local/sbin" "/usr/local/bin"
+fish_add_path --move "$HOME/.local/bin"
