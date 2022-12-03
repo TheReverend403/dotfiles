62491711a33c5507b9275add939b722bd213762e
Lee Watson <me@lwatson.dev>
2022-12-03 11:17:32 +0000

    Add mpd.socket for cleaner startup of yams, set GNUPGHOME to $XDG_CONFIG_HOME/gnupg

--- config/fish/conf.d/10-env.fish
+++ config/fish/conf.d/10-env.fish
@@ -1,6 +1,5 @@
 # XDG partial support.
 set -x ASPELL_CONF "per-conf $XDG_CONFIG_HOME/aspell/aspell.conf; personal $XDG_CONFIG_HOME/aspell/en.pws; repl $XDG_CONFIG_HOME/aspell/en.prepl"
-set -x NODE_REPL_HISTORY "$XDG_CACHE_HOME/node_repl_history"
 set -x SQLITE_HISTORY "$XDG_CACHE_HOME/sqlite_history"
 set -x PSQLRC "$XDG_CONFIG_HOME/pg/psqlrc"
 set -x PGSERVICEFILE "$XDG_CONFIG_HOME/pg/pg_service.conf"
@@ -12,6 +11,7 @@ set -x INPUTRC "$XDG_CONFIG_HOME/readline/inputrc"
 set -x MYSQL_HISTFILE "$XDG_DATA_HOME/mysql_history"
 set -x LESSHISTFILE -
 set -x PARALLEL_HOME "$XDG_CONFIG_HOME/parallel"
+set -x GNUPGHOME "$XDG_CONFIG_HOME/gnupg"
 
 # Prevent Wine adding menu entries and desktop links.
 set -x WINEDLLOVERRIDES 'winemenubuilder.exe=d'
@@ -33,6 +33,7 @@ set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
 fish_add_path "$CARGO_HOME/bin"
 
 # Node
+set -x NODE_REPL_HISTORY "$XDG_CACHE_HOME/node_repl_history"
 set -x NPM_PACKAGES "$XDG_DATA_HOME/npm"
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
 fish_add_path "$NPM_PACKAGES/bin"
