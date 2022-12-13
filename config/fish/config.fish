a9c28caeb45e46b9148b079b72180cdd47641c62
Lee Watson <me@lwatson.dev>
2022-12-13 16:09:01 +0000

    Switch to Sway

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,16 @@
 # Run X if not already running, not root, not in SSH and not in tmux
-if test -z "$DISPLAY" -a (id -u "$USER") -ne 0 -a -z "$SSH_CLIENT" -a -z "$TMUX" -a (tty) = "/dev/tty1"
-    set -l xlauncher startx
-    if command -q sx
-        set xlauncher sx
-    end
+# if test -z "$DISPLAY" -a (id -u "$USER") -ne 0 -a -z "$SSH_CLIENT" -a -z "$TMUX" -a (tty) = "/dev/tty1"
+#     set -l xlauncher startx
+#     if command -q sx
+#         set xlauncher sx
+#     end
 
-    flog "Starting X11 with $xlauncher..."
-    exec $xlauncher >/dev/null 2>&1
+#     flog "Starting X11 with $xlauncher..."
+#     exec $xlauncher >/dev/null 2>&1
+# end
+
+# Run Sway if not already running, not root, not in SSH and not in tmux
+if test -z "$WAYLAND_DISPLAY" -a (id -u "$USER") -ne 0 -a -z "$SSH_CLIENT" -a -z "$TMUX" -a (tty) = "/dev/tty1"
+    flog "Starting Sway..."
+    exec startsway
 end
