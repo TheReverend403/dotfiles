c7b0bb8258bf0797ef7bca84dcfaac8b623f163a
Lee Watson <me@lwatson.dev>
2023-01-13 11:32:51 +0000

    Use Fish 3.4's new theme thing for that, remove unused sway keys, switch mpv to pipewire natively

--- config/fish/conf.d/00-theme.fish
+++ config/fish/conf.d/00-theme.fish
@@ -1,14 +1,2 @@
 set -g fish_greeting
-set fish_color_param default
-set fish_color_command default 
-set fish_color_cwd magenta
-set fish_color_operator brblack
-set fish_color_end $fish_color_operator
-set fish_color_comment brblack --italics
-set fish_color_quote brgreen
-set fish_color_error red
-set fish_color_search_match --background=brblack
-set fish_color_escape white
-set fish_pager_color_prefix magenta
-set fish_pager_color_progress magenta
-set fish_pager_color_description white --italics
+fish_config theme choose lelianux
