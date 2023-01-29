aaa7fe01bd517459f2dfeb40ae20e4eda4875047
Lee Watson <me@lwatson.dev>
2023-01-29 23:08:55 +0000

    Switch to dep for neovim package management

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,13 @@
+require('bootstrap')
+require('plugins')
+
 local fn = vim.fn
 local o = vim.o
-local g = vim.g
 
-g.mapleader = ','
-
-o.shell = '/bin/bash'
-
-o.termguicolors = true
-
-o.cursorline = true
 o.report = 0
 
-o.number = true
 o.showmode = false
 o.modeline = false
-
-o.splitbelow = true
-o.splitright = true
 
 o.hlsearch = true
 
@@ -30,8 +21,4 @@ o.pastetoggle = '<F3>'
 
 o.mouse = false
 
-o.undofile = true
-o.backup = true
 o.backupdir = string.format('%s/backup', fn.stdpath('data'))
-
-require('plugins')
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,56 +1,39 @@
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd([[packadd packer.nvim]])
-        return true
-    end
-    return false
-end
-
-local packer_bootstrap = ensure_packer()
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-    use 'gpanders/editorconfig.nvim'
-    use 'haya14busa/is.vim'
-    use 'sheerun/vim-polyglot'
-    use {
+require('dep') {
+    'gpanders/editorconfig.nvim',
+    'haya14busa/is.vim',
+    'sheerun/vim-polyglot',
+    {
+        'echasnovski/mini.nvim',
+        function()
+            require('mini.basics').setup({
+                mappings = {
+                    basic = false
+                }
+            })
+            require('mini.comment').setup()
+            require('mini.surround').setup()
+            require('mini.pairs').setup()
+        end,
+    },
+    {
         'navarasu/onedark.nvim',
-        config = function()
+        function()
             local onedark = require('onedark')
             onedark.setup({
                 code_style = { comments = 'none' }
             })
             onedark.load()
-        end
-    }
-    use {
+        end,
+    },
+    {
         'nvim-lualine/lualine.nvim',
+        function()
+            require('lualine').setup({
+                options = { theme = 'onedark' },
+            })
+        end,
         requires = {
-            'navarasu/onedark.nvim',
             'nvim-tree/nvim-web-devicons'
         },
-        options = { theme = 'onedark' },
-        config = function()
-            require('lualine').setup()
-        end
     }
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
-
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-
-    vim.cmd([[
-        augroup packer_user_config
-            autocmd!
-            autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-        augroup end
-    ]])
-end)
+}
