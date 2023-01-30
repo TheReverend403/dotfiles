07c220a36ba6cfbc48325270b67111cd120c0d08
Lee Watson <me@lwatson.dev>
2023-01-30 14:41:28 +0000

    Remove unneeded C-Space binding

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -21,8 +21,6 @@ cmp.setup({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
-        ['<C-Space>'] = cmp.mapping.complete(),
-
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({
             select = false,
