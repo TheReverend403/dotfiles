f00ad6ac4e3c11f8dae2f02acd90bb2737b0de04
Lee Watson <me@lwatson.dev>
2023-07-19 14:31:52 +0100

    nvim: Make terminal and completion keymaps slightly more convenient.

--- config/nvim/lua/config/completion.lua
+++ config/nvim/lua/config/completion.lua
@@ -33,7 +33,7 @@ cmp.setup({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
-        ['<C-e>'] = cmp.mapping.abort(),
+        ['<esc>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({
             select = false,
         }),
