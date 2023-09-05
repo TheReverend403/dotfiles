abaae89b743dbadca66bc4edf68e1394d29ac951
Lee Watson <me@lwatson.dev>
2023-09-05 01:02:17 +0100

    Add playerctl binds, make sure theme vars are in the systemd environment before sway starts

--- config/sway/bin/import-gsettings.sh
+++ config/sway/bin/import-gsettings.sh
@@ -18,19 +18,20 @@ gsettings set "$gnome_schema" icon-theme "$icon_theme"
 gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
 gsettings set "$gnome_schema" cursor-size "$cursor_size"
 gsettings set "$gnome_schema" font-name "$font_name"
-swaymsg "font pango:$font_name"
+swaymsg -q "font pango:$font_name" || true
 
 # Set xcursor to match for xwayland.
 # Currently broken as per https://github.com/swaywm/sway/issues/6931
 # Works when applying the linked pull request.
 # https://github.com/swaywm/sway/pull/7064.patch
-swaymsg "seat * xcursor_theme $cursor_theme $cursor_size"
+swaymsg -q "seat * xcursor_theme $cursor_theme $cursor_size" || true
 
 # Also set the env vars for processes started by systemd eg. mako
 export GTK_THEME="$gtk_theme"
 export XCURSOR_THEME="$cursor_theme"
 export XCURSOR_SIZE="$cursor_size"
 dbus-update-activation-environment --systemd XCURSOR_THEME XCURSOR_SIZE GTK_THEME
+systemctl --user import-environment XCURSOR_THEME XCURSOR_SIZE GTK_THEME
 
 # Make flatpak GTK apps match the host.
 command -v flatpak >/dev/null 2>&1 && flatpak override --user --env=GTK_THEME="$gtk_theme"
