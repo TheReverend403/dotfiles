2f9d3beeb180b9eee59aac95907e3db5ca8ca7ce
Lee Watson <me@lwatson.dev>
2023-09-22 11:03:32 +0100

    Add cmp support to autopairs

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -4,4 +4,12 @@ return {
     check_ts = true,
     enable_check_bracket_line = false,
   },
+  config = function(_, opts)
+    local npairs = require("nvim-autopairs")
+    npairs.setup(opts)
+
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
 }
