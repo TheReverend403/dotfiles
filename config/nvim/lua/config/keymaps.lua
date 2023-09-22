4cd5b73964334e2702217b96c12da9f65a1818c8
Lee Watson <me@lwatson.dev>
2023-09-22 11:12:52 +0100

    Add tree leader keybinds

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,8 @@
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+
+vim.keymap.set("n", "<leader>q", ":qa!<CR>", {})
+vim.keymap.set("n", "<leader>s", ":w<CR>", {})
+
+vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>", {})
+vim.keymap.set("n", "<leader>nr", ":NvimTreeRefresh<CR>", {})
+vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>", {})
