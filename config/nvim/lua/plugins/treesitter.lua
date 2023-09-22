2af5af1773fc2d5fc06222670a6b551338686795
Lee Watson <me@lwatson.dev>
2023-09-22 01:58:05 +0100

    Remove treesitter textobjects

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@ return {
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
