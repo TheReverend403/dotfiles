61ab7906d9dc26563c9cb6c9e5b862b69449913f
Lee Watson <me@lwatson.dev>
2023-10-03 22:07:40 +0100

    Revert indentation to pre v3 style

--- config/nvim/lua/plugins/indent.lua
+++ config/nvim/lua/plugins/indent.lua
@@ -1,5 +1,15 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  opts = {},
+  opts = {
+    indent = {
+      char = "│",
+    },
+    scope = {
+      enabled = false,
+    },
+  },
+  config = function(_, opts)
+    require("ibl").setup(opts)
+  end,
 }
