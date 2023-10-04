94df8a62a335e3dcfb86f1d2d8d6b4d9ce453fc7
Lee Watson <me@lwatson.dev>
2023-10-04 22:58:06 +0100

    add neovim auto-session

---
+++ config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,13 @@
+return {
+  "rmagatti/auto-session",
+  opts = {
+    log_level = "error",
+    auto_session_suppress_dirs = {
+      "~/documents",
+      "~/documents/code",
+      "~/scratch",
+      "~/",
+      "/",
+    },
+  },
+}
