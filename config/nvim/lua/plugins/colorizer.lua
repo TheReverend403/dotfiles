89bed6dcd021433a0789d4d46ce293aeda6b842b
Lee Watson <me@lwatson.dev>
2023-10-04 17:37:01 +0100

    Switch to NvChad/nvim-colorizer.lua

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -1,10 +1,24 @@
+local css_common = {
+  names = true,
+  css = true,
+  css_fn = true,
+  sass = { enable = true },
+}
+
 return {
-  "norcalli/nvim-colorizer.lua",
+  "NvChad/nvim-colorizer.lua",
   opts = {
-    { "*" },
-    { names = false },
+    user_default_options = {
+      names = false,
+      mode = "virtualtext",
+    },
+    filetypes = {
+      "*",
+      css = css_common,
+      scss = css_common,
+      sass = css_common,
+      less = css_common,
+      "!lazy",
+    },
   },
-  config = function()
-    require("colorizer").setup()
-  end,
 }
