591a07d7c2d41ad1646c79961ec180ddc96680d3
Lee Watson <me@lwatson.dev>
2023-10-07 15:11:41 +0100

    Helps when you actually add the plugin file...

--- config/nvim/lua/helpers/buffers.lua
+++ config/nvim/lua/helpers/buffers.lua
@@ -1,28 +1,16 @@
 local M = {}
-local function noop()
-  print("NOOP")
+local close_buffers = require("close_buffers")
+
+M.delete_this = function()
+  close_buffers.delete({ type = "this", force = true })
 end
 
-local ok, close_buffers = pcall(require, "close_buffers")
-if ok then
-  M.delete_this = function()
-    close_buffers.delete({ type = "this", force = true })
-  end
+M.delete_all = function()
+  close_buffers.delete({ type = "all", force = true })
+end
 
-  M.delete_all = function()
-    close_buffers.delete({ type = "all", force = true })
-  end
-
-  M.delete_others = function()
-    close_buffers.delete({ type = "other", force = true })
-  end
-else
-  M.delete_this = function()
-    vim.cmd.bdelete()
-  end
-
-  M.delete_all = noop
-  M.delete_others = noop
+M.delete_others = function()
+  close_buffers.delete({ type = "other", force = true })
 end
 
 return M
---
+++ config/nvim/lua/plugins/close-buffers.lua
@@ -0,0 +1,4 @@
+return {
+  "kazhala/close-buffers.nvim",
+  opts = {},
+}
