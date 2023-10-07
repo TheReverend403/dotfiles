59380adb0a1f267a01090389bf13276480657af7
Lee Watson <me@lwatson.dev>
2023-10-07 15:10:00 +0100

    Revert close-buffer changes

--- config/nvim/lua/helpers/buffers.lua
+++ config/nvim/lua/helpers/buffers.lua
@@ -1,17 +1,28 @@
 local M = {}
-
-local close_buffers = require("close_buffers")
-
-M.delete_this = function()
-  close_buffers.delete({ type = "this", force = true })
+local function noop()
+  print("NOOP")
 end
 
-M.delete_all = function()
-  close_buffers.delete({ type = "all", force = true })
-end
+local ok, close_buffers = pcall(require, "close_buffers")
+if ok then
+  M.delete_this = function()
+    close_buffers.delete({ type = "this", force = true })
+  end
 
-M.delete_others = function()
-  close_buffers.delete({ type = "other", force = true })
+  M.delete_all = function()
+    close_buffers.delete({ type = "all", force = true })
+  end
+
+  M.delete_others = function()
+    close_buffers.delete({ type = "other", force = true })
+  end
+else
+  M.delete_this = function()
+    vim.cmd.bdelete()
+  end
+
+  M.delete_all = noop
+  M.delete_others = noop
 end
 
 return M
