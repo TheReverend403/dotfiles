96a58ba7c3bdd2aefa0cdbc74e86a6da8010ac5e
Lee Watson <me@lwatson.dev>
2024-10-08 18:42:55 +0100

    Remove text sources from neovim completions

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -7,6 +7,17 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
+    -- Disable annoying Text sources
+    opts.sources = vim.tbl_filter(function(source)
+      return not vim.tbl_contains({ "buffer", "nvim_lsp" }, source.name)
+    end, opts.sources)
+    table.insert(opts.sources, 1, {
+      name = "nvim_lsp",
+      entry_filter = function(entry, _)
+        return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
+      end,
+    })
+
     local luasnip = require("luasnip")
     local cmp = require("cmp")
 
