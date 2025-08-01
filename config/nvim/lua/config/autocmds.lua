acfc2195e653875429fdee613343e057a46b6997
Lee Watson <me@lwatson.dev>
2025-08-01 15:35:08 +0100

    Add sites-{enabled,available} to caddyfile detection

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,11 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*sway/conf.d/*" },
+  pattern = { "*/sway/conf.d/*" },
   command = "set filetype=swayconfig",
+})
+
+autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*/sites-{enabled,available}/*" },
+  command = "set filetype=caddyfile",
 })
